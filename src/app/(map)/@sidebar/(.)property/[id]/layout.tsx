be73354ddc3031e6631f0fac2b900d1c314ee0da
Jack Clackett <jack@noquarter.co>
2024-12-13 19:41:40 +0100

    layout

--- src/app/(map)/@sidebar/(.)property/[id]/layout.tsx
+++ src/app/(map)/@sidebar/(.)property/[id]/layout.tsx
@@ -4,11 +4,11 @@ import { BackButton } from "./back-button"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="fixed h-screen top-0 left-0 bottom-0 p-4">
-      <div className="relative flex-1 h-full rounded bg-background p-6 w-[400px] shadow-lg">
+      <div className="relative flex-1 h-full rounded bg-background p-6 w-[500px] overflow-y-scroll shadow-lg">
         <div className="absolute top-4 right-4">
           <BackButton />
         </div>
-        <div className="overscroll-y-scroll">{children}</div>
+        {children}
       </div>
     </div>
   )
