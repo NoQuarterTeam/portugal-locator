be75eb1b19b94834a4ec7376cd2f870d62d6e924
Jack Clackett <jack@noquarter.co>
2024-12-13 19:32:19 +0100

    loading

--- src/app/(map)/property/[id]/loading.tsx
+++ src/app/(map)/property/[id]/loading.tsx
@@ -1,9 +1,24 @@
 export default function Page() {
   return (
-    <div className="flex flex-col gap-2">
-      <div className="h-10 w-20 bg-border rounded animate-pulse" />
-      <div className="h-6 w-full bg-border rounded animate-pulse" />
-      <div className="h-6 w-full bg-border rounded animate-pulse" />
+    <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
+        <div className="h-6 w-32 bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+        <div className="h-12 w-full bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      </div>
+      <div className="flex flex-row gap-2">
+        <div className="h-6 w-12 bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+        <div className="h-6 w-20 bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      </div>
+      <div className="flex flex-row gap-2 overflow-scroll">
+        <div className="shrink-0 h-[200px] w-[200px] bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+        <div className="shrink-0 h-[200px] w-[200px] bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+        <div className="shrink-0 h-[200px] w-[200px] bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      </div>
+      <div className="h-6 w-full bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      <div className="h-6 w-full bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      <div className="h-6 w-full bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      <div className="h-6 w-full bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+      <div className="h-6 w-full bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
     </div>
   )
 }
