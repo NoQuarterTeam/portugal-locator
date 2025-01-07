85772d28834832fcc3d66368b7559d2b8903b2ee
Jack Clackett <jack@noquarter.co>
2025-01-07 15:07:51 +0100

    tweaks

--- src/app/(map)/property/[id]/layout.tsx
+++ src/app/(map)/property/[id]/layout.tsx
@@ -5,8 +5,8 @@ import type * as React from "react"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative p-8 w-[600px] mx-auto">
-      <div className="absolute top-4 right-4">
+    <div className="relative p-4 md:p-8 w-full max-w-[600px] mx-auto">
+      <div className="absolute top-2 right-2 md:top-4 md:right-4">
         <Button asChild size="icon" variant="ghost">
           <Link href="/">
             <XIcon size={20} />
